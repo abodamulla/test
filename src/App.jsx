5c482762361fa256741503267e8bc7e3b9bae0f7
karmo <karmo.mulla@gmail.com>
2021-06-02 15:00:49 +0300

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Form from './Components/Form/Form'
-import LastPage from './Components/Table/LastTable'
+import Page from './Components/Table/Table'
 
 // import {} from '@material-ui/core'
 // import {} from '@material-ui/icons'
@@ -12,7 +12,7 @@ function App() {
         <div className="app">
             <Router>
                 <Switch>
-                    <Route exact path="/"     component={LastPage} />
+                    <Route exact path="/"     component={Page} />
                     <Route       path="/Edit" component={Form} />
                 </Switch>
             </Router>
